38cb700b58f0ad3729034f78b143cd19811e30ee
unknown <paulrdelgado@gmail.com>
2018-06-17 19:25:20 -0700

    testing 5 counter

--- main.cpp
+++ main.cpp
@@ -11,6 +11,28 @@ SC_MODULE (hello_world) {
 	}
 };
 
+SC_MODULE (fivebitcounter) {
+	sc_in clk, reset, load;
+	sc_out > q;
+	sc_out > parallelin;
+
+	SC_CTOR(fivebitcounter) {
+		void clkme();
+		SC_METHOD(clkme);
+		sensitive << clk.pos() << reset.pos();
+	}
+
+	void clkme() {
+		if (reset.read()) q = 0;
+		else if (load.read()) q = parallelin.read();
+		else {
+			int nv = q.read() + 1;
+			printf("Five pending set to %i\n", nv);
+			q = nv;
+		}
+	}
+};
+
 int sc_main(int argc, char* argv[]) {
 	hello_world hello("HELLO");
 	hello.say_hello();
