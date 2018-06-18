74d08f9a924a58faad488a77ca40d4ee05ea4e37
unknown <paulrdelgado@gmail.com>
2018-06-17 20:03:07 -0700

    finished main program

--- main.cpp
+++ main.cpp
@@ -38,9 +38,62 @@ SC_MODULE (fivebitcounter) {
 	}
 };
 
+SC_MODULE(clkgen) {
+	sc_out <bool> clk;
+
+	SC_CTOR(clkgen) { SC_THREAD(runme);}
+
+	void runme() {
+		clk = 0;
+		while(1) {
+			cout << "Toggle at " << sc_time_stamp() << "\n";
+			wait(50, SC_NS);
+			clk = !clk.read();
+		}
+	}
+};
+
 int sc_main(int argc, char* argv[]) {
 	hello_world hello("HELLO");
 	hello.say_hello();
+
+	printf("fixed-field arithmetic/counter example:\n");
+
+	sc_tace_file *tf = sc_create_vcd_trace_file("trace");
+	tf->set_time_unit(1, SC_NS);
+
+	sc_signal <bool> clk, load, reset;
+	sc_signal <sc_uint <5> > parallelin, q;
+
+	clkgen u_clkgen("u_clkgen");
+	u_clkgen.clk(clk);
+
+	fivebitcounter u_fivebitcounter("u_fivebitcounter");
+	u_fivebitcounter.clk(clk);
+	u_fivebitcounter.parallelin(parallelin);
+	u_fivebitcounter.load(load);
+	u_fivebitcounter.q(q);
+	u_fivebitcounter.reset(reset);
+
+	sc_trace(tf, parallelin, "parallelin");
+	sc_trace(tf, q, "q");
+	sc_trace(tf, clk, "clk");
+	sc_trace(tf, load, "load");
+	sc_trace(tf, reset, "reset");
+
+	// useful but crude way of generating input stimulus
+	reset = 1;
+	parallelin = 0;
+	load = 0;
+	cout << "Exiting from reset at " << sc_time_stamp() << "\n";
+
+	sc_start(4000, SC_NS);
+	parallelin = 4;
+	load = 1;
+	cout << "Set load high at " << sc_time_stamp() << "\n";
+	sc_start(2000, SC_NS);
+
+	cout << "Finished at " << sc_start_stamp() << "\n";
 
 	return 0;
 }
